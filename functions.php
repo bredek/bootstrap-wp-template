ababc32183aa08e57f232ef1a4c09bdbdb8b9257
eugene.bredyuk <eugene.bredyuk@gmail.com>
2017-05-22 06:44:00 +0300

    Added scripts

--- functions.php
+++ functions.php
@@ -1,10 +1,25 @@
 <?php 
 
 function theme_styles(){
-    wp_enqueue_style( 'bootstrap_css', get_template_directory_uri() . '/css/bootstrap.min.css');    
-    wp_enqueue_style( 'main_css', get_template_directory_uri() . '/css/style.css');    
+    wp_enqueue_style( 'bootstrap_css', get_template_directory_uri() . '/css/bootstrap.min.css' );    
+    wp_enqueue_style( 'main_css', get_template_directory_uri() . '/css/style.css' );    
+}
+
+function theme_js(){
+
+    global $wp_scripts;
+
+    wp_register_script( 'html5_shiv', 'https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js', '', false );
+    wp_register_script( 'respond_js', 'https://oss.maxcdn.com/respond/1.4.2/respond.min.js', '', false );
+
+    //  loading additional IE 9 scripts if we have IE 9
+    $wp_scripts->add_data( 'html5_shiv', 'conditional', 'lt IE 9' );
+    $wp_scripts->add_data( 'respond_js', 'conditional', 'lt IE 9' );
+
+    wp_enqueue_script( 'bootstrap_js',  get_template_directory_uri() . '/js/bootstrap.min.js' , array('jquery'), true );
 }
 
 add_action( 'wp_enqueue_scripts' , 'theme_styles' );
+add_action( 'wp_enqueue_scripts' , 'theme_js' );
 
 ?>
